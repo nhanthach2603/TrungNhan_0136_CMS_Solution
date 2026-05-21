c347efd0502b2fd5e037ac3191814dbb5ee30e87
nhanthach2603 <thachtrantrungnhantdc@gmail.com>
2026-05-21 15:13:20 +0700

    buoi 2 21/05/2026

---
+++ CMS.Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿/*
+    Ho ten sinh vien: Thach Tran Trung Nhan
+    Ma sv: 2123110136
+    Ngay tao: 21 / 05 / 2026
+ */
+
+using Microsoft.EntityFrameworkCore;
+using CMS.Data.Entities;
+
+namespace CMS.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        // Khai báo các bảng dữ liệu
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<CategoryProduct> CategoriesProducts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
