cc5854641b5093696c4e764f29743c26750d2a5a
nhanthach <thachtrantrungnhantdc@gmail.com>
2026-05-21 15:13:20 +0700

    buoi 2 21/05/2026

--- CMS.Backend/Controllers/CategoryController.cs
+++ CMS.Backend/Controllers/CategoryController.cs
@@ -6,18 +6,23 @@
 
 
 
+using CMS.Data;
 using Microsoft.AspNetCore.Mvc;
-using CMS.Data.Entities; // Kết nối tới lớp dữ liệu bạn vừa tạo
 
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _context;
+
+    // "Tiêm" kết nối vào Controller
+    public CategoryController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
-        // Tạo danh sách dữ liệu mẫu trực tiếp trong code
-        var list = new List<Category> {
-            new Category { Id = 1, Name = "Tin Công Nghệ", Description = "Review Laptop, AI" },
-            new Category { Id = 2, Name = "Giáo Dục", Description = "Thông tin tuyển sinh" }
-        };
-        return View(list); // Gửi danh sách này sang giao diện
+        // Lấy dữ liệu THẬT từ bảng Categories trong SQL
+        var data = _context.Categories.ToList();
+        return View(data);
     }
 }
